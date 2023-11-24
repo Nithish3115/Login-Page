3069c811177f2ff06f00504e0b88be6787479e55
Nithish3115 <145634803+Nithish3115@users.noreply.github.com>
2023-11-24 21:07:00 +0530

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,77 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+     <!--navbar  -->
+    <header>
+        <nav class="container">
+            <div class="logo-box">
+                <a href="https://www.youtube.com/@TheCodeDonkey" target="_blank">
+                    <img src="/login.png" class="logo">
+                </a>
+            </div>
+
+            <div class="parent-link">
+                <a href="#"  class="social-links">About</a>
+                <a href="#" class="social-links" >Project</a>
+                 <a href="#" class="social-links">Experience</a>
+                <a href="#" class="social-links">Contact</a>
+            </div>
+
+            <div class="icon">
+                <a href="https://ww.instagram.com" target="_blank" class="sm-icon">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+                <a href="https://ww.twitter.com"  target="_blank" class="sm-icon">
+                    <ion-icon name="logo-twitter"></ion-icon>
+                </a>
+                <a href="https://ww.github.com" target="_blank"  class="sm-icon"> 
+                    <ion-icon name="logo-github"></ion-icon>
+                </a>
+                <a href="https://ww.linkdin.com" target="_blank"  class="sm-icon"> 
+                    <ion-icon name="logo-linkdin"></ion-icon>
+                </a>
+                <a href="https://ww.youtube.com" target="_blank"  class="sm-icon"> 
+                    <ion-icon name="logo-youtube"></ion-icon>
+                </a>
+            </div>
+        </nav>
+    </header>
+
+
+
+    <!-- login form -->
+    <div class="form">
+        <form action="#" class="sub-form">
+            <div class="upper-form">
+                <h2>Login Form</h2>
+                <label >Username</label><br>
+                <input type="text" name="username"><br>
+                <label >Password</label><br>
+                <input type="password"><br>
+                   <div class="btn">
+                <button type="submit" value="Login">Login</button><br>
+                     </div>
+
+            </div>
+
+            <div class="bottom-form">
+                <div class="no-account">Don't have an account?</div>
+                <a href="#" class="signup">Signup</a>
+            </div>
+
+        </form>
+    </div>
+     <script src="index.js"></script>
+ </body>
+</html>
+
+
+ 
